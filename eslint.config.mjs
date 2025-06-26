3c17280beb83cdd8f7d80e7571bb717416341a92
semanticdata <contact@miguelpimentel.do>
2025-06-26 11:11:54 -0500

    update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+	{
+		ignores: ['node_modules/', 'main.js'],
+	},
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+			parser: tseslint.parser,
+			parserOptions: {
+				sourceType: 'module',
+			},
+		},
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+			'@typescript-eslint/ban-ts-comment': 'off',
+			'no-prototype-builtins': 'off',
+			'@typescript-eslint/no-empty-function': 'off',
+			'@typescript-eslint/no-unused-expressions': 'off',
+		},
+	},
+);
